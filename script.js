9064af66c3d4c3d3199cbbda25afb5db30e36822
Anas Elkalla <79075490+AnasElkalla@users.noreply.github.com>
2025-10-30 18:28:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const share = document.querySelector("button");
 share.addEventListener("click", () => {
   //   popUp.style.display = "flex";
   popUp.classList.toggle("active");
+  share.classList.toggle("active-button");
 });
